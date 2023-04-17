59302dee504b1b250e59a567471a8be233e2ee90
Thalesgcostta <thalesgarcia.costa2@gmail.com>
2023-04-16 22:34:06 -0300

    16-04

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+$dbHost = 'containers-us-west-54.railway.app';
+$dbUsername = 'root';
+$dbPassword = 'ILmYePblIBW81yMnABaf';
+$dbName = 'railway';
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+//if($conexao->connect_errno)
+//{
+//    echo "Erro";
+//}
+//else
+//{
+//    echo "Conexão efetuada com sucesso";
+//}
+
+?>
